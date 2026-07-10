b85473a332cff267d724b99a3c109291d620b84a
MeghanaD20 <meghanapoojari514@gmail.com>
2026-07-10 21:26:32 +0530

    Update backend URL

--- server/server.js
+++ server/server.js
@@ -1,32 +1,14 @@
-const express = require("express");
-const cors = require("cors");
-require("dotenv").config();
+import axios from "axios";
 
-const healthRoutes = require("./routes/healthRoutes");
-const reviewRoutes = require("./routes/reviewRoutes");
-
-const app = express();
-
-app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://ai-code-review-assistant-azure-tau.vercel.app"
-  ],
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-}));
-
-app.use(express.json());
-
-app.use("/api/health", healthRoutes);
-app.use("/api/review", reviewRoutes);
-
-app.get("/", (req, res) => {
-  res.send("🚀 AI Code Review Assistant Backend is Running!");
+const API = axios.create({
+  baseURL: "https://ai-code-review-assistant-8btc.onrender.com/api",
 });
 
-const PORT = process.env.PORT || 5000;
+export const reviewCode = async (code, language) => {
+  const response = await API.post("/review", {
+    code,
+    language,
+  });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+  return response.data;
+};
