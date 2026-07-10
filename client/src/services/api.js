3ee51de2eaad74bb11c3348e686994f4598dcabb
MeghanaD20 <meghanapoojari514@gmail.com>
2026-07-10 16:21:11 +0530

    Update API URL for production

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://ai-code-review-assistant-8bto.onrender.com/api",
 });
 
 export const reviewCode = async (code, language) => {
